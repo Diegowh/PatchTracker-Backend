82c70eea23239e46c06c7e9c8574210d8ad6e441
Diegowh <contacto.diegowh@gmail.com>
2023-05-31 17:46:09 +0200

    created models.py

--- ValorantPatchTracker/models.py
+++ ValorantPatchTracker/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class PatchNote(models.Model):
+    version = models.CharField(max_length=10)
+    release_date = models.DateField()
+    general_updates = models.TextField()
+    known_issues = models.TextField()
+
+class AgentUpdate(models.Model):
+    patch_note = models.ForeignKey(PatchNote, on_delete=models.CASCADE)
+    agent_name = models.CharField(max_length=50)
+    update_details = models.TextField()
+
+class CosmeticUpdate(models.Model):
+    patch_note = models.ForeignKey(PatchNote, on_delete=models.CASCADE)
+    update_details = models.TextField()
+
+class SystemExperienceUpdate(models.Model):
+    patch_note = models.ForeignKey(PatchNote, on_delete=models.CASCADE)
+    update_details = models.TextField()
+
+class BugFix(models.Model):
+    patch_note = models.ForeignKey(PatchNote, on_delete=models.CASCADE)
+    fix_details = models.TextField()
