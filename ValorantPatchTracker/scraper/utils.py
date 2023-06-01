37e00377871f434b9af37607632a133f47b3a1a8
Diegowh <contacto.diegowh@gmail.com>
2023-06-01 22:01:47 +0200

    fixed docstring

--- ValorantPatchTracker/scraper/utils.py
+++ ValorantPatchTracker/scraper/utils.py
@@ -2,7 +2,5 @@ PATCH_V = "3.08"
 BASE_URL = "https://valorant.fandom.com/wiki/Patch_Notes/"
 
 def url_generator(patch_version: str):
-    """
-    Generate a URL for the patch notes of a specific version of Valorant
-    """
+    """Generates a URL for the patch notes of a specific version of Valorant"""
     return f"{BASE_URL}{patch_version}"
