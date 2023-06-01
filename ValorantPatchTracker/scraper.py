67187c522508fb35a407921b07a49de15c8a35ef
Diegowh <contacto.diegowh@gmail.com>
2023-06-01 20:14:13 +0200

    Modified Scraper

--- ValorantPatchTracker/scraper.py
+++ ValorantPatchTracker/scraper.py
@@ -27,17 +27,32 @@ class Scraper():
 
 
     def is_comment(self, element):
+        '''Check if the element is a comment.'''
         return isinstance(element, Comment)
     
     
     def soup_cleaner(self):
+        '''Clean up the soup object and return the cleaned soup'''
         
         if self.soup is None:
             print('Soup object is None, unable to clean')
             return None
         
         div_main = self.soup.find('div', class_='mw-parser-output')
+        tags_to_delete = ['svg', 'script', 'img']
+        tags_to_replace = ['span', 'a']
+        self._get_patch_elements(div_main)
+        self._remove_tags(div_main, tags_to_delete)
+        self._replace_tags(div_main, tags_to_replace)
+        self._remove_comments(div_main)
+        self._remove_review_div(div_main)
+        
+        print(div_main)
+        
+        return self.soup
 
+    def _get_patch_elements(self, div_main):
+        '''Remove elements until h2 tag is found'''
         for element in div_main:
             if isinstance(element, NavigableString):
                 continue
@@ -45,39 +60,39 @@ class Scraper():
                 break
             else:
                 element.decompose()
+        
+    def _remove_tags(self, div_main: NavigableString, tags: list):
+        '''Remove specific tags.
+        
+        Args:
+            div_main (BeautifulSoup object): The div main object to clean.
+            tags (list): List of tags to remove
+        '''
+        for tag in tags:
+            for element in div_main(tag):
+                element.decompose()
                 
+    def _replace_tags(self, div_main: NavigableString, tags: list):
+        '''Replace tags with their text content.'''
         
-        for svg in div_main('svg'):
-            svg.decompose()
-            
-        for script in div_main('script'):
-            script.decompose()
-            
-        for img in div_main('img'):
-            img.decompose()
-            
-        for span in div_main.find_all('span'):
-            span.replace_with(span.get_text())
-        
-        for a in div_main.find_all('a'):
-            a.replace_with(a.get_text())
-            
-        for a in div_main('a'):
-            a.decompose()
-            
+        for tag in tags:
+            for element in div_main.find_all(tag):
+                element.replace_with(element.get_text())
+                
+    def _remove_comments(self, div_main):
+        '''Remove comments from div_main'''
         comments = div_main.find_all(text=self.is_comment)
         for comment in comments:
             comment.extract()
             
+    def _remove_review_div(self, div_main):
+        '''Removes div.reviews'''
         review_div = div_main.find('div', class_='reviews')
         try:
             review_div.decompose()
-        except TypeError:
+        except TypeError as e:
+            print(f'div.reviews not found: {e}')
             pass
-            
-        print(div_main)
-
-        return self.soup
 
 url = url_generator(PATCH_V)
 scraper = Scraper(url)
