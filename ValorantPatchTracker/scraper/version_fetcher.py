88693275324608cc34245eefe2e7bd28e93079b8
Diegowh <contacto.diegowh@gmail.com>
2023-06-02 03:44:15 +0200

    Created episodes method for VersionFetcher. Functional!!

--- ValorantPatchTracker/scraper/version_fetcher.py
+++ ValorantPatchTracker/scraper/version_fetcher.py
@@ -9,19 +9,33 @@ class VersionFetcher:
         self.url = URL
         self.soup = soup(self.url)
         
-    def versions(self):
-        versions = []
+    def episodes(self):
         center = self.soup.find('center')
-        
+        episodes = []
         p = center.find('p')
         p.extract()
         
         for table in center.find_all('table'):
-            version_data = {
+            episode_data = {
                 "episode_name": table.find('caption').text.replace('\n', ''),
+                "versions": [],
             }
-            print(version_data)
-
+            for row in table.find_all('tr')[1:]: # ignore header row
+                cols = row.find_all('td')
+                if len(cols) == 3:
+                    patch = cols[0].text.strip()
+                    patch_endpoint = cols[0].find('a')['href'] if cols[0].find('a') else None
+                    release_date = cols[1].text.strip()
+                    
+                    version = {
+                        "patch": patch,
+                        "patch_endpoint": patch_endpoint,
+                        "release_date": release_date,
+                    }
+                    episode_data["versions"].append(version)
+            episodes.append(episode_data)
+        
+        print(episodes)
 
 version_fetcher = VersionFetcher()
-version_fetcher.versions()
+version_fetcher.episodes()
