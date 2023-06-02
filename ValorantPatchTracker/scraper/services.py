c4682786ac15d291e5320ff88352d062a4d7c2b6
Diegowh <contacto.diegowh@gmail.com>
2023-06-03 00:41:06 +0200

    created scrap_data function, returns html from all patches in a list

--- ValorantPatchTracker/scraper/services.py
+++ ValorantPatchTracker/scraper/services.py
@@ -6,5 +6,12 @@ def scrap_data():
     episodes = VersionFetcher().episodes()
     patch_endpoints = [version["patch_endpoint"] for episode in episodes for version in episode["versions"]]
     
+    all_html = []
     for endopoint in patch_endpoints:
         scraper = Scraper(endopoint)
+        patch_html = scraper.patch_html
+        all_html.append(patch_html)
+        
+    return all_html
+
+print(scrap_data())
