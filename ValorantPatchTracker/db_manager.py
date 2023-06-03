a81d21c30061fff69b95e1581a9ba5a844c2164c
Diegowh <contacto.diegowh@gmail.com>
2023-06-03 19:20:03 +0200

    Created store_content method for DBManager

--- ValorantPatchTracker/db_manager.py
+++ ValorantPatchTracker/db_manager.py
@@ -7,4 +7,9 @@ class DBManager:
     def __init__(self) -> None:
         self.episodes_data = all_episodes_data()
         
-        
+    def store_content(self, patch_note, html_content):
+        '''Stores content data into the database'''
+        Content.objects.create(
+            patch_note=patch_note,
+            html_content=html_content,
+        )
