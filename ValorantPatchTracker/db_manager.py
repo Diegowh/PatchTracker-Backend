d6588c0d273184854a1e149232313bdf0377a3a3
Diegowh <contacto.diegowh@gmail.com>
2023-06-03 19:42:37 +0200

    Added creat_episodes to DBManager

--- ValorantPatchTracker/db_manager.py
+++ ValorantPatchTracker/db_manager.py
@@ -7,13 +7,17 @@ class DBManager:
     def __init__(self) -> None:
         self.episodes_data = all_episodes_data()
         
-    def create_content(self, patch_note, html_content):
-        '''Creates content data for the database'''
-        Content.objects.create(
-            patch_note=patch_note,
-            html_content=html_content,
-        )
-        
+    def create_episodes(self):
+        for episode_data in self.episodes_data:
+            episode_name = episode_data['episode_name']
+            episode, created = Episode.objects.get_or_create(
+                episode_name=episode_name,
+            )
+            
+            if created:
+                self.create_patch_notes(episode, episode_data['versions'])
+                
+                
     def create_patch_notes(self, episode, versions):
         for version_data in versions:
             version = version_data['patch']
@@ -23,6 +27,14 @@ class DBManager:
                 release_date=release_date,
                 episode=episode,
             )
-
+            
             if created:
                 self.create_content(patch_note, version_data['content'])
+                
+                
+    def create_content(self, patch_note, html_content):
+        '''Creates content data for the database'''
+        Content.objects.create(
+            patch_note=patch_note,
+            html_content=html_content,
+        )
