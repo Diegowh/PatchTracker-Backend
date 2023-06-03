f9d135d8826bd1ee68919715b815b7e0728dff29
Diegowh <contacto.diegowh@gmail.com>
2023-06-03 19:46:44 +0200

    Added a _ to DBManager methods who are not created to be called outside the class

--- ValorantPatchTracker/db_manager.py
+++ ValorantPatchTracker/db_manager.py
@@ -1,11 +1,12 @@
 from .models import Episode, PatchNote, Content
 from .scraper.services import all_episodes_data
 
+
 class DBManager:
     def __init__(self) -> None:
         self.episodes_data = all_episodes_data()
         
-    def create_episodes(self):
+    def _create_episodes(self):
         for episode_data in self.episodes_data:
             episode_name = episode_data['episode_name']
             episode, created = Episode.objects.get_or_create(
@@ -16,7 +17,7 @@ class DBManager:
                 self.create_patch_notes(episode, episode_data['versions'])
                 
                 
-    def create_patch_notes(self, episode, versions):
+    def _create_patch_notes(self, episode, versions):
         for version_data in versions:
             version = version_data['patch']
             release_date = version_data['release_date']
@@ -30,7 +31,7 @@ class DBManager:
                 self.create_content(patch_note, version_data['content'])
                 
                 
-    def create_content(self, patch_note, html_content):
+    def _create_content(self, patch_note, html_content):
         '''Creates content data for the database'''
         Content.objects.create(
             patch_note=patch_note,
