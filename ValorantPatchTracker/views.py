9dd8a3c9232ddefb2419b66d09e26de7cde0f2a7
Diegowh <diegogarcia2312@gmail.com>
2023-06-05 16:00:57 +0200

    Make new imports for views.py

--- ValorantPatchTracker/views.py
+++ ValorantPatchTracker/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Episode, PatchNote, Content
+from .serializers import EpisodeSerializer, PatchNoteSerializer, ContentSerializer
