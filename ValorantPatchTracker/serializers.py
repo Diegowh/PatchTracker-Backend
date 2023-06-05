f2c912ccaf27901dd96cf63eb4977c00996ae543
Diegowh <diegogarcia2312@gmail.com>
2023-06-05 15:57:37 +0200

    Created serializers.py to define ModelSerializer for each table in order to use Django Rest Framework API

---
+++ ValorantPatchTracker/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Episode, PatchNote, Content
+
+class EpisodeSerializer(serializers.ModelSerializer):
+    pass
+
+class PatchNoteSerializer(serializers.ModelSerializer):
+    pass
+
+class ContentSerializer(serializers.ModelSerializer):
+    pass
