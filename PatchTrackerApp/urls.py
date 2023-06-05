0ba8ecf793e7d8f8ff89a701937300a2ce2df01f
Diegowh <diegogarcia2312@gmail.com>
2023-06-05 16:10:56 +0200

    Fixed: Wrongly included path to project urls. Now its correctly included.

--- PatchTrackerApp/urls.py
+++ PatchTrackerApp/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('PatchTrackerApp', include('PatchTrackerApp.urls')),
+    path('ValorantPatchTracker', include('ValorantPatchTracker.urls')),
 ]
