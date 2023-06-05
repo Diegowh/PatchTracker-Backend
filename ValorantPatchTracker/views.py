e77b5913f252a1bfc228e6eb520d0663244db2c5
Diegowh <diegogarcia2312@gmail.com>
2023-06-05 16:03:37 +0200

    Create views

--- ValorantPatchTracker/views.py
+++ ValorantPatchTracker/views.py
@@ -3,3 +3,16 @@ from rest_framework import viewsets
 
 from .models import Episode, PatchNote, Content
 from .serializers import EpisodeSerializer, PatchNoteSerializer, ContentSerializer
+
+
+class EpisodeView(viewsets.ModelViewSet):
+    queryset = Episode.objects.all()
+    serializer_class = EpisodeSerializer
+
+class PatchNoteView(viewsets.ModelViewSet):
+    queryset = PatchNote.objects.all()
+    serializer_class = PatchNoteSerializer
+
+class ContentView(viewsets.ModelViewSet):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
