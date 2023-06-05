b01cf25815cd178cd2c2e58f2615d9d2f953a6a9
Diegowh <diegogarcia2312@gmail.com>
2023-06-05 15:03:05 +0200

    Create a function who gives basic css style to the patch_html. Its needed because of the way I am rendering the html in the views on SwiftUI.

--- ValorantPatchTracker/scraper/services.py
+++ ValorantPatchTracker/scraper/services.py
@@ -9,7 +9,29 @@ def all_episodes_data():
     for episode in episodes:
         for version in episode['versions']:
             scraper = Scraper(version['patch_endpoint'])
-            version['content'] = scraper.patch_html.strip()
+            version['content'] = style_html(scraper.patch_html.strip())
             # version['content'] = "<h1>HTML PLACEHOLDER</h1>"
         
     return episodes
+
+def style_html(content):
+    return f"""<!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    body {{
+        font-family: -apple-system, sans-serif;
+        font-size: 35px;
+        line-height: 1.6;
+        padding: 20px;
+        border: 2px solid #000000;
+    }}
+    </style>
+    </head>
+    <body>
+    {content}
+    </body>
+    </html>
+    """
+    
+    
