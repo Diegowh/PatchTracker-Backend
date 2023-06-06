66549676c298ead28b37670151aeae5fb79cb807
Diegowh <diegogarcia2312@gmail.com>
2023-06-06 13:27:08 +0200

    Delete filter backend

--- ValorantPatchTracker/views.py
+++ ValorantPatchTracker/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from .models import Episode, PatchNote, Content
@@ -9,17 +8,11 @@ from .serializers import EpisodeSerializer, PatchNoteSerializer, ContentSerializ
 class EpisodeView(viewsets.ModelViewSet):
     queryset = Episode.objects.all()
     serializer_class = EpisodeSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['episode']
 
 class PatchNoteView(viewsets.ModelViewSet):
     queryset = PatchNote.objects.all()
     serializer_class = PatchNoteSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['patchnote']
 
 class ContentView(viewsets.ModelViewSet):
     queryset = Content.objects.all()
     serializer_class = ContentSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['content']
