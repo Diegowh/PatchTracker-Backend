18a643819d301b9a3d2f25d5f432db5a9afae2ad
Diegowh <contacto.diegowh@gmail.com>
2023-06-08 17:27:48 +0200

    Create models

--- LoLPatchTracker/models.py
+++ LoLPatchTracker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Season(models.Model):
+    name = models.TextField()
+
+class Patch(models.Model):
+    season = models.ForeignKey(Season, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    release_date = models.TextField()
+    
+class Notes(models.Model):
+    patch = models.ForeignKey(Patch, on_delete=models.CASCADE)
+    html = models.TextField()
