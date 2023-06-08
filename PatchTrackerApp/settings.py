aa71aa26db2431f1e1354245623833f7bf14f668
Diegowh <diegogarcia2312@gmail.com>
2023-06-08 14:00:49 +0200

    Use decouple.config to access the .env for the SECRET_KEY

--- PatchTrackerApp/settings.py
+++ PatchTrackerApp/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure---&l4ud_0k8))!(j3y8oe&d00z@d=&_nu!5^k2mo*&p9rdy6+%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
