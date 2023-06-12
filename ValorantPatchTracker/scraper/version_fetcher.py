7943184396c975c56d46b1363408812a7e4a8b00
Diegowh <contacto.diegowh@gmail.com>
2023-06-12 15:04:23 +0200

    Rename VersionFetcher

--- ValorantPatchTracker/scraper/version_fetcher.py
+++ ValorantPatchTracker/scraper/version_fetcher.py
@@ -3,7 +3,7 @@ from .utils import soup
 
 URL = "https://valorant.fandom.com/wiki/Patch_Notes"
 
-class VersionFetcher:
+class ValorantVersionFetcher:
     def __init__(self) -> None:
         self.url = URL
         self.soup = soup(self.url)
