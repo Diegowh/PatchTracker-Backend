34a4524877ecbcf36bce7faa3b8090be9807dfe5
Diegowh <contacto.diegowh@gmail.com>
2023-06-12 13:27:20 +0200

    Modify models.py in order to store the date of each season and the new champions released for patch

--- LoLPatchTracker/models.py
+++ LoLPatchTracker/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 class Season(models.Model):
     name = models.TextField()
+    date = models.TextField()
 
 class Patch(models.Model):
     season = models.ForeignKey(Season, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
-    release_date = models.TextField()
+    date = models.TextField()
+    new_champion = models.TextField(null=True, blank=True)
     
 class Notes(models.Model):
     patch = models.ForeignKey(Patch, on_delete=models.CASCADE)
