c0bb26b30a957c7445ae8519d9be5414c8e0ffc8
Diegowh <contacto.diegowh@gmail.com>
2023-06-12 17:07:05 +0200

    Create utils.py in root project to migrate both utils.py from LolPatchTracker and ValorantPatchTracker to avoid redudant code.

---
+++ PatchTrackerApp/utils.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def patch_url_generator(base_url:str, patch_endpoint: str):
+    """Generates a URL for the patch notes of a specific version of Valorant"""
+    return f"{base_url}{patch_endpoint}"
+
+
+def soup(url):
+    """Fetches the webpage from the given URL and parses it using BeautifulSoup
+
+    Returns:
+        BeautifulSoup object: Parsed HTML of the webpage
+    """
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f'Request to {url} failed: e')
+        return None
+    
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    return soup
