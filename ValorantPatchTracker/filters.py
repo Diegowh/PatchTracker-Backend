dd3df41f9409ab10181ada778e7e11140b5e3bb9
Diegowh <contacto.diegowh@gmail.com>
2023-06-12 16:33:08 +0200

    Del

--- ValorantPatchTracker/filters.py
+++ ValorantPatchTracker/filters.py
@@ -4,4 +4,4 @@ from .models import Content
 class ContentFilter(django_filters.FilterSet):
     class Meta:
         model = Content
-        fields = ['patch_note']  # Aquí especificas los campos por los que quieres filtrar.
+        fields = ['patch_note']
