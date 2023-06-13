c5937fe782e4e78ae6a10cb435133e5819a3ffdd
Diegowh <contacto.diegowh@gmail.com>
2023-06-13 03:06:14 +0200

    FIX. Recursion error. Now _seasons returns a dict of all season names with their endpoints since Season Nine. It avoids _seaso

--- LoLPatchTracker/scraper/scraper.py
+++ LoLPatchTracker/scraper/scraper.py
@@ -4,10 +4,11 @@ from django.conf import settings
 
     
 class SeasonScraper:
-    def _seasons(self) -> list:
-        '''Returns a list of all season names since Season Nine'''
-        seasons = [season for season in self._get_season_endpoints()]
-        return seasons[:-10]
+    def _seasons(self) -> dict:
+        '''Returns a dict of all season names and their endpoints since Season Nine'''
+        all_season_endpoints =self._get_season_endpoints()
+        valid_seasons = list(all_season_endpoints.keys())[:-10]
+        return {season: all_season_endpoints[season] for season in valid_seasons}
         
     def _find_seasons_ul(self):
         '''Finds Seasons ul tag from soup'''
@@ -18,14 +19,12 @@ class SeasonScraper:
         return ul.find_all('li')
     
     def _get_season_endpoints(self):
-        '''Returns a dictionary with all seasons names and their endpoint.'''
+        '''Returns a dictionary with all seasons endpoint.'''
         endpoint_dict = {}
         for li in self._find_seasons_ul():
             a_tag = li.find('a')
             if a_tag:
                 endpoint_dict[a_tag.text] = a_tag.get('href')
-            else:
-                print("No A tag found.")
         return endpoint_dict
 
 
--- LoLPatchTracker/views.py
+++ LoLPatchTracker/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .scraper.scraper import LoLScraper
+import json
 
 def test_view(request):
     test_endpoint = "/Season_Thirteen"
     scraper = LoLScraper()
-    result = []
-    for season in scraper._seasons():
-        result.append(season + ', ')
-    
-    return HttpResponse(result)
+    result = scraper._seasons()
+    return HttpResponse(json.dumps(result), content_type="application/json")
