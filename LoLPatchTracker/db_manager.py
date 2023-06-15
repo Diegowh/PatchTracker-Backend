220d7f2e25efceb5fed7a75ad975a37fe6c2b9f2
Diegowh <contacto.diegowh@gmail.com>
2023-06-15 14:01:12 +0200

    Change get_or_create use to update_or_create

--- LoLPatchTracker/db_manager.py
+++ LoLPatchTracker/db_manager.py
@@ -8,11 +8,11 @@ class DBManager:
         
     def _create_seasons(self):
         for season_name in self.all_data.keys():
-            season, created = Season.objects.get_or_create(name=season_name)
-            if created:
-                first_patch = self.all_data[season_name][0]
-                season.date = first_patch['date']
-                season.save()
+            first_patch = self.all_data[season_name][-1]
+            season, _ = Season.objects.update_or_create(
+                name=season_name,
+                defaults={'date': first_patch['date']}
+            )
     
     def _create_patches(self):
         pass
