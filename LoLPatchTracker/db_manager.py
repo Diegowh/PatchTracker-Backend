881668db4c762f10670e75b38d4b46a9a2519b3a
Diegowh <contacto.diegowh@gmail.com>
2023-06-15 13:52:39 +0200

    Create db_manager.py class structure

--- LoLPatchTracker/db_manager.py
+++ LoLPatchTracker/db_manager.py
@@ -1,2 +1,19 @@
 from .scraper.scraper import LoLScraper
 from .models import Season, Patch, Notes
+
+
+class DBManager:
+    def __init__(self) -> None:
+        self.all_data = LoLScraper().all_seasons_data
+        
+    def _create_seasons(self):
+        pass
+    
+    def _create_patches(self):
+        pass
+    
+    def _create_notes(self):
+        pass
+    
+    def update(self):
+        pass
