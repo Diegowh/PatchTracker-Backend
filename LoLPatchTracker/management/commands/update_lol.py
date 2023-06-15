4c4311e7c04a17bbdd9ff0b54c60cb1575224fc5
Diegowh <contacto.diegowh@gmail.com>
2023-06-15 14:13:13 +0200

    Create the command to update LoLPatchTracker database.

--- LoLPatchTracker/management/commands/update_lol.py
+++ LoLPatchTracker/management/commands/update_lol.py
@@ -1,2 +1,11 @@
 from django.core.management.base import BaseCommand
 from ...db_manager import DBManager
+
+
+class Command(BaseCommand):
+    help = 'Updates database with the scraped data'
+    
+    def handle(self, *args, **options):
+        db_manager = DBManager()
+        db_manager.update()
+        self.stdout.write(self.style.SUCCESS('League of Legends Database sucessfully updated'))
