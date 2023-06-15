504bbde3f07dbfa7559e967a384b59cced4891d7
Diegowh <contacto.diegowh@gmail.com>
2023-06-15 13:54:45 +0200

    Create method to update Season model.

--- LoLPatchTracker/db_manager.py
+++ LoLPatchTracker/db_manager.py
@@ -7,7 +7,12 @@ class DBManager:
         self.all_data = LoLScraper().all_seasons_data
         
     def _create_seasons(self):
-        pass
+        for season_name in self.all_data.keys():
+            season, created = Season.objects.get_or_create(name=season_name)
+            if created:
+                first_patch = self.all_data[season_name][0]
+                season.date = first_patch['date']
+                season.save()
     
     def _create_patches(self):
         pass
