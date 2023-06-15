89bf221ae93d6a8a5a5135711a7435b591a8be68
Diegowh <contacto.diegowh@gmail.com>
2023-06-15 15:34:12 +0200

    Add time to update_db command to see execution times.

--- LoLPatchTracker/management/commands/update_db.py
+++ LoLPatchTracker/management/commands/update_db.py
@@ -1,11 +1,26 @@
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
+import time
 
 class Command(BaseCommand):
     help = 'Updates both databases.'
     
     def handle(self, *args, **options):
-        call_command('update_lol')
-        call_command('update_valorant')
+        start_time_total = time.time()
         
-        self.stdout.write(self.style.SUCCESS('Both Databases sucessfully updated'))
+        start_time_lol = time.time()
+        call_command('update_lol')
+        end_time_lol = time.time()
+        lol_execution_time = round((end_time_lol - start_time_lol), 2)
+        self.stdout.write(self.style.SUCCESS(f'in {lol_execution_time} seconds'))
+        
+        start_time_val = time.time()
+        call_command('update_valorant')
+        end_time_val = time.time()
+        val_execution_time = round((end_time_val - start_time_val), 2)
+        self.stdout.write(self.style.SUCCESS(f'in {val_execution_time} seconds'))
+        
+        
+        end_time_total = time.time()
+        total_execution_time = round((end_time_total - start_time_total), 2)
+        self.stdout.write(self.style.SUCCESS(f'Both Databases successfully updated in {total_execution_time} seconds'))
