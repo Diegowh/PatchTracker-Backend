f0ec86f83e45a298571de6d0244f738711a04a6c
Diegowh <contacto.diegowh@gmail.com>
2023-06-16 12:39:57 +0200

    Create high level router to structure API endpoints way more organized.

--- PatchTrackerApp/urls.py
+++ PatchTrackerApp/urls.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
+from ValorantPatchTracker.views import EpisodeView, PatchNoteView, ContentView
+from LoLPatchTracker.views import SeasonView, PatchView, NotesView
+
+router = DefaultRouter()
+router.register(r'valorant/episodes', EpisodeView)
+router.register(r'valorant/patchnotes', PatchNoteView)
+router.register(r'valorant/contents', ContentView)
+router.register(r'lol/seasons', SeasonView)
+router.register(r'lol/patches', PatchView)
+router.register(r'lol/notes', NotesView)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('valorantpatchtracker/', include('ValorantPatchTracker.urls')),
-    path('lolpatchtracker/', include('LoLPatchTracker.urls')),
+    path('api/', include(router.urls)),
 ]
