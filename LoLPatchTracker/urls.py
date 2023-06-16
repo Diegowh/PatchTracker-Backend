c782d0c0270b1e60c0041353f5d3c1ccd8b57a23
Diegowh <contacto.diegowh@gmail.com>
2023-06-16 12:43:55 +0200

    Delete unnecesary urls and routers from both apps since they are called on root/urls

--- LoLPatchTracker/urls.py
+++ LoLPatchTracker/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path, include
-from .views import test_view, SeasonView, PatchView, NotesView
-from rest_framework.routers import DefaultRouter
+from .views import test_view
 
-router = DefaultRouter()
-
-router.register(r'seasons', SeasonView)
-router.register(r'patches', PatchView)
-router.register(r'notes', NotesView)
 
 urlpatterns = [
     path('test/', test_view),
-    path('', include(router.urls)),
 ]
--- ValorantPatchTracker/urls.py
+++ ValorantPatchTracker/urls.py
@@ -1,16 +1,1 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-
-from .views import EpisodeView, ContentView, PatchNoteView
-
-
-router = DefaultRouter()
-
-router.register(r'episodes', EpisodeView)
-router.register(r'patchnotes', PatchNoteView)
-router.register(r'contents', ContentView)
-
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
