810e4cf7228941978abf6318fe7c054cea9cd5e8
Diegowh <contacto.diegowh@gmail.com>
2023-06-16 12:03:02 +0200

    Create serializers.

--- LoLPatchTracker/serializers.py
+++ LoLPatchTracker/serializers.py
@@ -1,2 +1,18 @@
 from rest_framework import serializers
 from .models import Season, Patch, Notes
+
+
+class SeasonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Season
+        fields = '__all__'
+        
+class PatchSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patch
+        fields = '__all__'
+        
+class NotesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notes
+        fields = '__all__'
