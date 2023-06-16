a4f4821f6bae43fd86b46df5899f19d28d229d71
Diegowh <contacto.diegowh@gmail.com>
2023-06-16 12:16:15 +0200

    Create router object. Register router for each view. Include path on urlpatterns.

--- LoLPatchTracker/urls.py
+++ LoLPatchTracker/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from .views import test_view, SeasonView, PatchView, NotesView
 from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+
+router.register(r'seasons', SeasonView)
+router.register(r'patches', PatchView)
+router.register(r'notes', NotesView)
+
 urlpatterns = [
     path('test/', test_view),
+    path('', include(router.urls)),
 ]
