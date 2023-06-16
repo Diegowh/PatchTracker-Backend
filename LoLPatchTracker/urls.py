f8cd7b8a4cb17ecabaa788f1be3a96866f33fe00
Diegowh <contacto.diegowh@gmail.com>
2023-06-16 12:14:16 +0200

    Modify imports for urls.py

--- LoLPatchTracker/urls.py
+++ LoLPatchTracker/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import test_view
+from .views import test_view, SeasonView, PatchView, NotesView
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('test/', test_view),
