5067429314ce723cc626cf254482dba6051d6060
Diegowh <contacto.diegowh@gmail.com>
2023-06-16 12:31:36 +0200

    Delete unnecesary variable

--- LoLPatchTracker/views.py
+++ LoLPatchTracker/views.py
@@ -8,7 +8,6 @@ from .models import Season, Patch, Notes
 from . serializers import SeasonSerializer, PatchSerializer, NotesSerializer
 
 def test_view(request):
-    test_endpoint = "/Season_Thirteen"
     scraper = LoLScraper()
     result = scraper.all_seasons_data
     return HttpResponse(json.dumps(result), content_type="application/json")
