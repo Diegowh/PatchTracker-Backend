e6fc203b9b97ca875f271d019cf057c30d895160
Diegowh <contacto.diegowh@gmail.com>
2023-06-18 14:11:21 +0200

    Create filter for PatchNote model[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[DFilterSet for PatchNote model.

--- ValorantPatchTracker/filters.py
+++ ValorantPatchTracker/filters.py
@@ -1,7 +1,13 @@
 import django_filters
-from .models import Content
+from .models import Content, PatchNote
 
 class ContentFilter(django_filters.FilterSet):
     class Meta:
         model = Content
         fields = ['patch_note']
+
+
+class PatchNoteFilter(django_filters.FilterSet):
+    class meta:
+        model = PatchNote
+        fields = ['episode']
