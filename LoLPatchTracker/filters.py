9bae332716a2de118886d9641364e8e486323038
Diegowh <contacto.diegowh@gmail.com>
2023-06-18 14:06:35 +0200

    FIX typo, now its correctly inheriting from django_filters.FilterSet

--- LoLPatchTracker/filters.py
+++ LoLPatchTracker/filters.py
@@ -1,13 +1,13 @@
 import django_filters
 from .models import Patch, Notes
 
-class PatchFilter(django_filters.filterset):
+class PatchFilter(django_filters.FilterSet):
     class Meta:
         model = Patch
         fields = ['season']
         
         
-class NotesFilter(django_filters.filterset):
+class NotesFilter(django_filters.FilterSet):
     class Meta:
         model = Notes
         fields = ['patch']
