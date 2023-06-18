5693d604eb89d814453d94a71398fa1840ccb91b
Diegowh <contacto.diegowh@gmail.com>
2023-06-18 14:01:17 +0200

    Create Filterset classes

--- LoLPatchTracker/filters.py
+++ LoLPatchTracker/filters.py
@@ -1,2 +1,13 @@
 import django_filters
 from .models import Patch, Notes
+
+class PatchFilter(django_filters.filterset):
+    class Meta:
+        model = Patch
+        fields = ['season']
+        
+        
+class NotesFIlter(django_filters.filterset):
+    class Meta:
+        model = Notes
+        fields = ['patch']
